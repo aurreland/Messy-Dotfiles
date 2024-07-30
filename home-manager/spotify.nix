7ca57879b6d8585139d4665029bf01e554067a99
Aurelien Andreo <aurelien.andreo@proton.me>
2024-07-30 14:31:27 +0200

    Update spotify.nix

--- home-manager/spotify.nix
+++ home-manager/spotify.nix
@@ -2,6 +2,16 @@
   system,
   inputs,
   ...
-}: {
-  home.packages = [inputs.spicetify-nix.packages.${system}.default];
+}: let
+  spicePkgs = inputs.spicetify-nix.packages.${system}.default;
+in {
+  imports = [inputs.spicetify-nix.homeManagerModule];
+
+  programs.spicetify = {
+    enable = true;
+    theme = spicePkgs.themes.catppuccin;
+    enabledCustomApps = with spicePkgs.apps; [
+      marketplace
+    ];
+  };
 }
