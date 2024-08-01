2b25039e0f4a9331a981bf26a18e97881ddfeeac
Aurelien Andreo <aurelien.andreo@proton.me>
2024-08-01 08:57:21 +0200

    Update material-maker.nix

--- modules/material-maker.nix
+++ modules/material-maker.nix
@@ -21,7 +21,7 @@
   libXrandr,
   libXrender,
   zlib,
-  udev, # for libudev
+  udev,
 }:
 stdenv.mkDerivation rec {
   pname = "material-maker";
@@ -32,8 +32,14 @@ stdenv.mkDerivation rec {
     hash = "sha256-Y8+ZwXy3zqnsxqqaZeVgFSxLzmUkq+rBzbq8tEDc8/g=";
   };
 
+  icon = fetchurl {
+    url = "https://github.com/RodZill4/material-maker/raw/1.3/icon.png";
+    hash = "sha256-cu4aD8WbeYuISeub0Mt7Z/2e4/9BZF+QwsqYFo82Ge8=";
+  };
+
   nativeBuildInputs = [
     autoPatchelfHook
+    copyDesktopItems
     godot3-headless
   ];
 
@@ -51,6 +57,18 @@ stdenv.mkDerivation rec {
     libXrender
     zlib
     udev
+  ];
+
+  desktopItems = [
+    (makeDesktopItem {
+      name = "material-maker";
+      exec = "material-maker";
+      icon = "material-maker";
+      desktopName = "Material Maker";
+      comment = meta.description;
+      categories = ["Graphics" "Development"];
+      keywords = ["material"];
+    })
   ];
 
   buildPhase = ''
@@ -76,6 +94,9 @@ stdenv.mkDerivation rec {
 
     install -D -m 755 ./src/material_maker_1_3_linux/material_maker.x86_64 $out/libexec/material-maker
     install -D -m 644 ./src/material_maker_1_3_linux/material_maker.pck $out/libexec/material-maker.pck
+
+    mkdir -p $out/share/icons/hicolor/256x256/apps/
+    install -D -m 644 $icon -t $out/share/icons/hicolor/256x256/apps/
 
     cp -r ./src/material_maker_1_3_linux/doc $out/libexec
     chmod -R 755 $out/libexec/doc
@@ -106,9 +127,9 @@ stdenv.mkDerivation rec {
 
   meta = with lib; {
     homepage = "https://www.materialmaker.org/";
-    description = "";
-    license = with licenses; [blueOak100];
+    description = "Tool based on Godot Engine that can be used to create textures procedurally and paint 3D models.";
+    license = licenses.mit;
     platforms = ["x86_64-linux"];
-    maintainers = with maintainers; [jojosch];
+    maintainers = with maintainers; [aurreland];
   };
 }
